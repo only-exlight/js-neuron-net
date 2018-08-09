d1a0f74868b6d64f5147d8b9d5da33e5ef412762
Andrey Kernichniy <akernichniy@sinergo.ru>
2018-08-09 23:47:49 +0700

    Add traning images

---
+++ src/consts/trainingSet.const.ts
@@ -0,0 +1,9 @@
+import { TrainingExample } from '../interfaces/TrainingExample.interface';
+
+export const trainingSet: TrainingExample[] = [{
+    image: 'training/1jpg',
+    isSquare: true
+}, {
+    image: 'training/2jpg',
+    isSquare: false
+}];
---
+++ src/interfaces/TrainingExample.interface.ts
@@ -0,0 +1,4 @@
+export interface TrainingExample {
+    isSquare: boolean,
+    image: string;
+}
